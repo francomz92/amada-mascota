08fdcf4f3ddfded4ad3d0a40d241c812285a24a7
Magui Brollo <maguieb@hotmail.com>
2021-02-27 21:21:12 -0300

    Varios en admin DJ

--- main/admin.py
+++ main/admin.py
@@ -9,25 +9,34 @@ Adopcion, Perdido, Encontro, tiene_notificacion
 class MascotaAdmin(admin.ModelAdmin):
     list_display = ["nombre", "especie","sexo","id_dueño", "foto"]
     list_filter = ["especie","id_dueño"]
-    #list_filter = ["especie","username"]
-   
-
-    def foto(self, obj):
-        #return "<img src=" +obj.fotos.url +"/>"
-        return format_html("<img src=%s width=130 height=auto />"%(obj.fotos.url))
- 
-class PerdidoAdmin(admin.ModelAdmin):
-    list_display = ["fecha_publicacion","fecha_evento"]
-    list_filter = ["fecha_evento"]
-    date_hierarchy = "fecha_publicacion"
     
 
+    def foto(self, obj):
+        return format_html("<img src=%s width=130 height=auto />"%(obj.fotos.url))
+ 
+
+class PerdidoAdmin(admin.ModelAdmin):
+    list_display = ["id_usuario","nombre_mascota","color_mascota", "foto_mascota", "fecha_publicacion","fecha_evento", "gratificacion"]
+    list_filter = ["id_usuario__username", "id_mascota__color","id_mascota__especie"]
+    date_hierarchy = "fecha_publicacion" #filtro avanzado para campo fecha y hora
+    
+    def nombre_mascota(self, obj):
+        return "%s"%(obj.id_mascota.nombre)
+    nombre_mascota.short_description ="Nombre Mascota"
+
+    def color_mascota(self, obj):
+        return "%s"%(obj.id_mascota.color)
+    color_mascota.short_description ="Color Mascota"
+    
+    def foto_mascota(self, obj):
+        return format_html("<img src=%s width=130 height=auto />"%(obj.id_mascota.fotos.url))
+    color_mascota.short_description ="Foto Mascota"
 
 admin.site.register(Contacto)
 admin.site.register(Ubicacion)
 admin.site.register(Mascota, MascotaAdmin)
 admin.site.register(Notificacion)
-admin.site.register(Publicacion)
+#admin.site.register(Publicacion) no debería mostrarse SOLA, debE ir acOmpañado de PERDIDO, ENCONTRÓ o ADOPCION
 admin.site.register(Adopcion)
 admin.site.register(Perdido, PerdidoAdmin)
 admin.site.register(Encontro)
