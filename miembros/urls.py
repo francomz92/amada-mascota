7bdd7f6c77ac726b1157ab066812d36e0e97c47c
enzo <enzostefani507@gmail.com>
2021-03-12 16:08:17 -0300

    recuperar contraseña olvidada

--- miembros/urls.py
+++ miembros/urls.py
@@ -1,24 +1,36 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import login, password_reset, password_reset_done,password_reset_confirm, password_reset_complete
+from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.RegistroUsuario.as_view(), name='register'),
     path('accounts/login/', views.LoginUsuario.as_view(), name='login'),
     path('perfil/',views.perfil,name="perfil"),
     path('editar/',views.perfilActualizar,name='editar'),
-    path('reset/password_reset',password_reset,
-        {'template_name':'registration/password_reset_form.html',
-        'email_template_name':'registrarion/password_reset_email.html'},
-        name='password_reset'),
-    path('reset/password_reset_done',password_reset_done,
-        {'template_name':'registration/password_reset_done.html'},
-        name='password_reset_done'),
-    path('reset/<uidb64>[0-9A-Za-z_\-]+/<token>.+',password_reset_confirm,
-        {'template_name':'registration/password_reset_confirm.html'},
-        name='password_reset_confirm'),
-    path('reset/done',password_reset_complete,
-        {'template_name':'registration/password_reset_complete.html'},
-        name='password_reset_complete_done')
+    
+    path('password/change/', 
+        auth_views.PasswordChangeView.as_view(), 
+        name='password_change'),
+
+    path('password/change/done/',
+            auth_views.PasswordChangeDoneView.as_view(), 
+            name='password_change_done'),
+    
+    path('password/reset/', 
+            auth_views.PasswordResetView.as_view(), 
+            name='password_reset'),
+
+    path('password/reset/done/', 
+            auth_views.PasswordResetDoneView.as_view(), 
+            name='password_reset_done'),
+
+    path('password/reset/<uidb64>/<token>/', 
+            auth_views.PasswordResetConfirmView.as_view(), 
+            name='password_reset_confirm'),
+
+    path('password/reset/complete/', 
+            auth_views.PasswordResetCompleteView.as_view(), 
+            name='password_reset_complete'),
 ]
 
