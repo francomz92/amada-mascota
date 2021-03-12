31e7bfe2c6d3372a1e7e4aacf6f4402b17085e5c
Franco <franco.mz92@hotmail.com>
2021-03-11 22:26:37 -0300

    Notificacion por email en encotrados

--- apps/encontrados/views.py
+++ apps/encontrados/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from apps.perdidos.models import Publicacion, Mascota, Ubicacion, Encontro
+from apps.perdidos.models import Publicacion, Mascota, Ubicacion, Encontro, tiene_notificacion, Notificacion
 from .forms import MascotaForm, UbicacionForm, EncontroForm, SearchForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -7,6 +7,11 @@ from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.utils import timezone
 from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.core import mail
+from django.core.mail import EmailMultiAlternatives#, EmailMessage, send_mail
+# from email.mime.image import MIMEImage
 
 # Create your views here.
 
@@ -38,7 +43,36 @@ def publicar(request):
          enc.id_mascota = masc
          enc.id_ubicacion = ubic
          enc.save()
-      
+         
+         user_notificacion = Notificacion.objects.filter(especie=masc.especie, localidad=ubic.localidad, tipo=Encontro)
+         to_email = [usuario.id_usuario.email for usuario in user_notificacion if usuario.fecha_hasta <= enc.fecha_publicacion]
+         html_content =f'''
+         <div style="display: flex; justify-content: center; align-items: center; width: 300px;"><div><span>Localidad: <strong>{ubic.localidad}</strong></span>&nbsp;&nbsp;&nbsp;&nbsp;<span>Barrio: <strong>{ubic.barrio}</strong></span></div>
+         <img src="{masc.fotos}" style="width: 100%;"></div>'''
+         
+         with mail.get_connection() as conexion:
+            email = EmailMultiAlternatives(
+               subject = '[Amada Mascota] Suscripción a Encontrados',
+               body = f'Acaban de publicar un {masc.especie} en {ubic.localidad}, ve a ver',
+               from_email = settings.EMAIL_HOST_USER,
+               to = to_email,
+               connection= conexion
+            )
+            email.attach_alternative(html_content, "text/html")
+            email.send()
+         
+         '''
+         with mail.get_connection() as conexion:
+            send_mail(
+               subject = '[Amada Mascota] Suscripción a Encontrados',
+               message = f'Acaban de publicar un {masc.especie} en {ubic.localidad}, ve a ver',
+               from_email = settings.EMAIL_HOST_USER,
+               recipient_list = to_email,
+               fail_silently = False,
+               html_message = html_content,
+               connection = conexion
+            )
+          '''  
          return redirect(to='encontrados:lista_encontrados')
       else:
          messages.error(request, 'Ups...parece que algo salió mal.!! Vuelve a intentarlo.')
